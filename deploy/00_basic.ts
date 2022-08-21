b6c598d01ae6b92efe762e2c705e6e90cd65446b
Dan Finlay <dan@danfinlay.com>
2022-08-21 16:20:50 -0700

    Add enforcers to deploy script

--- deploy/00_basic.ts
+++ deploy/00_basic.ts
@@ -12,4 +12,53 @@ export default async function deploy(hardhat: HardhatRuntimeEnvironment) {
     skipIfAlreadyDeployed: true,
     log: true,
   });
+
+  await deploy("AllowedMethodsEnforcer", {
+    contract: "AllowedMethodsEnforcer",
+    from: deployer,
+    args: [],
+    skipIfAlreadyDeployed: true,
+    log: true,
+  });
+
+  await deploy("BlockNumberEnforcer", {
+    contract: "BlockNumberEnforcer",
+    from: deployer,
+    args: [],
+    skipIfAlreadyDeployed: true,
+    log: true,
+  });
+
+  await deploy("ERC20AllowanceEnforcer", {
+    contract: "ERC20AllowanceEnforcer",
+    from: deployer,
+    args: [],
+    skipIfAlreadyDeployed: true,
+    log: true,
+  });
+
+  await deploy("LimitedCallsEnforcer", {
+    contract: "LimitedCallsEnforcer",
+    from: deployer,
+    args: [],
+    skipIfAlreadyDeployed: true,
+    log: true,
+  });
+
+  await deploy("TimestampEnforcer", {
+    contract: "TimestampEnforcer",
+    from: deployer,
+    args: [],
+    skipIfAlreadyDeployed: true,
+    log: true,
+  });
+
+  await deploy("EIP1271Enforcer", {
+    contract: "EIP1271Enforcer",
+    from: deployer,
+    args: [],
+    skipIfAlreadyDeployed: true,
+    log: true,
+  });
+
 }
