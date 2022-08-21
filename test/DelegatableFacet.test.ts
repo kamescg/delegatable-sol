e7c56e7de5bcef620a0d416643653b0a54f478c1
Dan Finlay <dan@danfinlay.com>
2022-08-20 21:02:24 -0700

    Change name of main test var

--- test/DelegatableFacet.test.ts
+++ test/DelegatableFacet.test.ts
@@ -69,21 +69,34 @@ describe("DelegatableFacet", () => {
     ]);
 
     // Add purpose facet to the Diamond
-    Diamond.diamondCut([{
-      facetAddress: PurposeFacet.address,
-      action: FacetCutAction.Add,
-      functionSelectors: getSelectors(PurposeFacet),
-    }], "0x0", "0x0");
+    Diamond.diamondCut(
+      [
+        {
+          facetAddress: PurposeFacet.address,
+          action: FacetCutAction.Add,
+          functionSelectors: getSelectors(PurposeFacet),
+        },
+      ],
+      "0x0",
+      "0x0"
+    );
 
     // Generate delegatable init code to generate domain typehash
-    const initTypehashBytes = DelegatableFacet.populateTransaction.setDomainHash(CONTACT_NAME);
+    const initTypehashBytes =
+      DelegatableFacet.populateTransaction.setDomainHash(CONTACT_NAME);
     // Add delegatable facet to the diamond
-    Diamond.diamondCut([{
-      facetAddress: DelegatableFacet.address,
-      action: FacetCutAction.Add,
-      functionSelectors: getSelectors(DelegatableFacet),
-    //   - run delegatable init code to generate domain typehash
-    }], DelegatableFacet.address, initTypehashBytes);
+    Diamond.diamondCut(
+      [
+        {
+          facetAddress: DelegatableFacet.address,
+          action: FacetCutAction.Add,
+          functionSelectors: getSelectors(DelegatableFacet),
+          //   - run delegatable init code to generate domain typehash
+        },
+      ],
+      DelegatableFacet.address,
+      initTypehashBytes
+    );
 
     CONTRACT_INFO = {
       chainId: Diamond.deployTransaction.chainId,
@@ -101,14 +114,14 @@ describe("DelegatableFacet", () => {
         "0x0000000000000000000000000000000000000000000000000000000000000000",
       caveats: [],
     };
-    expect(await Delegatable.getDelegationTypedDataHash(DELEGATION)).to.eq(
+    expect(await Diamond.getDelegationTypedDataHash(DELEGATION)).to.eq(
       "0x5352e474c0624192d7bdd9ace20cca8e397aa705676ef4f494218dcd291aac36"
     );
   });
   it("READ getInvocationsTypedDataHash(Invocations memory invocations)", async () => {
     const _delegation = generateDelegation(
       CONTACT_NAME,
-      Delegatable,
+      Diamond,
       pk0,
       wallet1.address
     );
@@ -121,17 +134,17 @@ describe("DelegatableFacet", () => {
         {
           authority: [_delegation],
           transaction: {
-            to: Delegatable.address,
+            to: Diamond.address,
             gasLimit: "21000000000000",
             data: (
-              await Delegatable.populateTransaction.setPurpose("To delegate!")
+              await Diamond.populateTransaction.setPurpose("To delegate!")
             ).data,
           },
         },
       ],
     };
     expect(
-      await Delegatable.getInvocationsTypedDataHash(INVOCATION_MESSAGE)
+      await Diamond.getInvocationsTypedDataHash(INVOCATION_MESSAGE)
     ).to.eq(
       "0xf6e94ae88b8b72d51444924d7cf26f28b4eaf7d5d274dffbdc83cb92cb4eeac5"
     );
@@ -140,18 +153,18 @@ describe("DelegatableFacet", () => {
   it("READ verifyDelegationSignature(SignedDelegation memory signedDelegation)`", async () => {
     const _delegation = generateDelegation(
       CONTACT_NAME,
-      Delegatable,
+      Diamond,
       pk0,
       wallet1.address
     );
-    expect(await Delegatable.verifyDelegationSignature(_delegation)).to.eq(
+    expect(await Diamond.verifyDelegationSignature(_delegation)).to.eq(
       wallet0.address
     );
   });
   it("READ verifyInvocationSignature(SignedInvocation memory signedInvocation)", async () => {
     const _delegation = generateDelegation(
       CONTACT_NAME,
-      Delegatable,
+      Diamond,
       pk0,
       wallet1.address
     );
@@ -164,49 +177,49 @@ describe("DelegatableFacet", () => {
         {
           authority: [_delegation],
           transaction: {
-            to: Delegatable.address,
+            to: Diamond.address,
             gasLimit: "21000000000000",
             data: (
-              await Delegatable.populateTransaction.setPurpose("To delegate!")
+              await Diamond.populateTransaction.setPurpose("To delegate!")
             ).data,
           },
         },
       ],
     };
     const invocation = delegatableUtils.signInvocation(INVOCATION_MESSAGE, pk0);
-    expect(await Delegatable.verifyInvocationSignature(invocation)).to.eq(
+    expect(await Diamond.verifyInvocationSignature(invocation)).to.eq(
       wallet0.address
     );
   });
 
   describe("contractInvoke(Invocation[] calldata batch)", () => {
     it("should SUCCEED to EXECUTE batched Invocations", async () => {
-      expect(await Delegatable.purpose()).to.eq("What is my purpose?");
+      expect(await Diamond.purpose()).to.eq("What is my purpose?");
       const _delegation = generateDelegation(
         CONTACT_NAME,
-        Delegatable,
+        Diamond,
         pk0,
         wallet1.address
       );
-      await Delegatable.contractInvoke([
+      await Diamond.contractInvoke([
         {
           authority: [_delegation],
           transaction: {
-            to: Delegatable.address,
+            to: Diamond.address,
             gasLimit: "21000000000000",
             data: (
-              await Delegatable.populateTransaction.setPurpose("To delegate!")
+              await Diamond.populateTransaction.setPurpose("To delegate!")
             ).data,
           },
         },
       ]);
-      expect(await Delegatable.purpose()).to.eq("To delegate!");
+      expect(await Diamond.purpose()).to.eq("To delegate!");
     });
   });
 
   describe("invoke(SignedInvocation[] calldata signedInvocations)", () => {
     it("should SUCCEED to EXECUTE a single Invocation from an unsigned authority", async () => {
-      expect(await Delegatable.purpose()).to.eq("What is my purpose?");
+      expect(await Diamond.purpose()).to.eq("What is my purpose?");
       const INVOCATION_MESSAGE = {
         replayProtection: {
           nonce: "0x01",
@@ -216,10 +229,10 @@ describe("DelegatableFacet", () => {
           {
             authority: [],
             transaction: {
-              to: Delegatable.address,
+              to: Diamond.address,
               gasLimit: "21000000000000",
               data: (
-                await Delegatable.populateTransaction.setPurpose("To delegate!")
+                await Diamond.populateTransaction.setPurpose("To delegate!")
               ).data,
             },
           },
@@ -229,20 +242,20 @@ describe("DelegatableFacet", () => {
         INVOCATION_MESSAGE,
         pk0
       );
-      await Delegatable.invoke([
+      await Diamond.invoke([
         {
           signature: invocation.signature,
           invocations: invocation.invocations,
         },
       ]);
-      expect(await Delegatable.purpose()).to.eq("To delegate!");
+      expect(await Diamond.purpose()).to.eq("To delegate!");
     });
 
     it("should SUCCEED to EXECUTE batched SignedInvocations", async () => {
-      expect(await Delegatable.purpose()).to.eq("What is my purpose?");
+      expect(await Diamond.purpose()).to.eq("What is my purpose?");
       const _delegation = generateDelegation(
         CONTACT_NAME,
-        Delegatable,
+        Diamond,
         pk0,
         wallet1.address
       );
@@ -255,10 +268,10 @@ describe("DelegatableFacet", () => {
           {
             authority: [_delegation],
             transaction: {
-              to: Delegatable.address,
+              to: Diamond.address,
               gasLimit: "21000000000000",
               data: (
-                await Delegatable.populateTransaction.setPurpose("To delegate!")
+                await Diamond.populateTransaction.setPurpose("To delegate!")
               ).data,
             },
           },
@@ -268,13 +281,13 @@ describe("DelegatableFacet", () => {
         INVOCATION_MESSAGE,
         pk0
       );
-      await Delegatable.invoke([
+      await Diamond.invoke([
         {
           signature: invocation.signature,
           invocations: invocation.invocations,
         },
       ]);
-      expect(await Delegatable.purpose()).to.eq("To delegate!");
+      expect(await Diamond.purpose()).to.eq("To delegate!");
     });
   });
 });
